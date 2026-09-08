5d864539b53316684c12ab69bb0f85e2575eeb5b
TrongPhucX5 <lphuc2324@gmail.com>
2026-09-08 19:20:27 +0700

    Kết nối Database với Spring JPA và tạo API CRUD

---
+++ src/main/java/com/trongphuc/identity_service/IdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trongphuc.identity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(IdentityServiceApplication.class, args);
+	}
+
+}
