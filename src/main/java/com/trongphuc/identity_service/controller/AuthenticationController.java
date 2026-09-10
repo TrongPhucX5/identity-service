9ecb9f15cea16ffd68da16019b872aa2e4d04d7f
TrongPhucX5 <lphuc2324@gmail.com>
2026-09-10 12:20:53 +0700

    issue JWT token

--- src/main/java/com/trongphuc/identity_service/controller/AuthenticationController.java
+++ src/main/java/com/trongphuc/identity_service/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.trongphuc.identity_service.controller;
 
+import com.nimbusds.jose.JOSEException;
 import com.trongphuc.identity_service.dto.request.ApiResponse;
 import com.trongphuc.identity_service.dto.request.AuthenticationRequest;
+import com.trongphuc.identity_service.dto.request.IntrospectRequest;
 import com.trongphuc.identity_service.dto.response.AuthenticationResponse;
+import com.trongphuc.identity_service.dto.response.IntrospectResponse;
 import com.trongphuc.identity_service.service.AuthenticationService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
+
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -19,13 +24,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     AuthenticationService authenticationService;
 
-    @PostMapping("/log-in")
+    @PostMapping("/token")
     ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
-        boolean result = authenticationService.authenticate(request);
+        var result = authenticationService.authenticate(request);
         return ApiResponse.<AuthenticationResponse>builder()
-                .result(AuthenticationResponse.builder()
-                        .authenticated(result)
-                        .build())
+                .result(result)
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    ApiResponse<IntrospectResponse> authenticate(@RequestBody IntrospectRequest request)
+            throws ParseException, JOSEException {
+        var result = authenticationService.introspect(request);
+        return ApiResponse.<IntrospectResponse>builder()
+                .result(result)
                 .build();
     }
 }
