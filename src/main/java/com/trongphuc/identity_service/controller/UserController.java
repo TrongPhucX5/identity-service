2e17a45ef01e532696d0fa05591fba1a5c7ef514
TrongPhucX5 <lphuc2324@gmail.com>
2026-09-11 22:55:00 +0700

    config Spring Security

--- src/main/java/com/trongphuc/identity_service/controller/UserController.java
+++ src/main/java/com/trongphuc/identity_service/controller/UserController.java
@@ -9,11 +9,13 @@ import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/users")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
